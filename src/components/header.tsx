e6c69370110052862090d6dbaf8cca746c2eb24b
Rafael Goncalves <rafaelgon.tech@gmail.com>
2024-12-23 12:10:37 -0300

    Update style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className="shadow-background fixed top-0 z-50 flex w-screen items-center justify-evenly py-2 shadow-md backdrop-blur-xl">
+    <div className="shadow-background border-b-[1px] border-accents border-opacity-30 fixed top-0 z-50 flex w-screen items-center justify-evenly py-2 shadow-md backdrop-blur-xl">
       <Image
         src="/assets/Logo.svg"
         alt=""
